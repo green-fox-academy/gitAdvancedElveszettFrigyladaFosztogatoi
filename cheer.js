d38374673110b1f73c7f2e10542c22c94967a208
Balazs Hegedus <hegedusb93@gmail.com>
2019-01-14 15:10:34 +0100

    cheer.js updated

--- cheer.js
+++ cheer.js
@@ -1,8 +1,4 @@
 let teamname = 'JavaScript'
 console.log(teamname);
 
-<<<<<<< HEAD
 console.log('ne szarozzá Józsi')
-=======
-console.log('Ez egy szar')
->>>>>>> c10701c0185ae97e6fa19ea8329c9b8847c158a1

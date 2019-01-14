950a80e4ae3d4b3fcae8faf97cf84ddd03b94603
Grafl Andras <grand.golyamail@gmail.com>
2019-01-14 12:11:16 +0100

    d2

--- future.js
+++ future.js
@@ -1,1 +1,9 @@
+//create a method (this will be referenced as your method 
+//from now on), called cheer in a file/class of your choice that 
+//prints out the team name
 alert("cancer");
+
+//modify your method to have one parameter and print that out
+
+let a = "Parameter";
+alert(a);

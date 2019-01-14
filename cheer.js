9b447b16b82459d4290a98aa6f714cdf081c6562
Márton Szél <marton.szel@gmail.comi>
2019-01-14 15:01:21 +0100

    hoppácska

--- cheer.js
+++ cheer.js
@@ -1,17 +1,1 @@
-let teamname = 'JavaScript'
-console.log(teamname);
-
-'SOROSAN KAPCSOLT GYÖRGY'
-
-
-alert (`Cheer for the ${teamname}`);
-=======
-
-'egy'
-
-'kettő'
-
-<<<<<<< HEAD
-"mutika"
-=======
->>>>>>> ae7cfdfb5c432b249628a992127288c3f7044c4f
+'hoppácska'
